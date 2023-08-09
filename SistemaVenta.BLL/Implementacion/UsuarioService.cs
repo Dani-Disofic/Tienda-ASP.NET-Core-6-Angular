95601433c29c4116c1cb14dc58ff5181df13a25c
Dani-Disofic <disoficdani@gmail.com>
2023-08-09 17:58:04 +0200

    implementada la clase javascript para el manejo CRUD de usuarios

--- SistemaVenta.BLL/Implementacion/UsuarioService.cs
+++ SistemaVenta.BLL/Implementacion/UsuarioService.cs
@@ -115,6 +115,7 @@ namespace SistemaVenta.BLL.Implementacion
                 usuario_editar.Correo= entidad.Correo;
                 usuario_editar.Telefono= entidad.Telefono;
                 usuario_editar.IdRol = entidad.IdRol;
+                usuario_editar.EsActivo = entidad.EsActivo;
 
                 if(usuario_editar.NombreFoto == "")
                 {
